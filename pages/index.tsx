371af186309661b703282b1ee8632066101d5b29
mjovano <127967109+mjovano@users.noreply.github.com>
2024-11-21 14:23:30 +0100

    fix

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+const Index = () => {
+    return (
+      <div>
+        <h1>Welcome to our page!</h1>
+        <p>Use the navigation links to browse to your liking!</p>
+      </div>
+    );
+  };
+  
+  export default Index;
